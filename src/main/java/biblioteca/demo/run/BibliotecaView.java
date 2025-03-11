9eda7c74b30ee48b6c6e628d0f437cb85e9cb6ae
EmmaAvaca <emmanuel.avaca@gmail.com>
2025-03-11 10:24:20 +0100

    11/032/2025

---
+++ src/main/java/biblioteca/demo/run/BibliotecaView.java
@@ -0,0 +1,33 @@
+package biblioteca.demo.run;
+
+import javax.swing.JFrame;
+
+public class BibliotecaView {
+
+	
+	protected JFrame frmBiblioteca;  //el JFrame es la ventana
+	
+	public BibliotecaView() {
+		initialize();
+		
+	}
+	private void initialize() {
+		
+		frmBiblioteca = new JFrame();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
